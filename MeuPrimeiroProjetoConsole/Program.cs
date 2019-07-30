7809b81b609512b45dcabe1dbde8807ffb5abf43
Amanda Lins <amandalinsmusic@hotmail.com>
2019-07-30 19:21:04 -0300

    O que eu quiser

---
+++ MeuPrimeiroProjetoConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjetoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (Int32 contador = 0; contador < 5; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
